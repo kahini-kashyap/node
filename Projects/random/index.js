c9c3359c5d82e9378a8baa533f997aa6fb25660d
Kahini Kashyap <jili@Kahinis-MacBook-Air.local>
2021-07-28 18:25:46 +0530

    28/07/21

--- Projects/random/index.js
+++ Projects/random/index.js
@@ -1,12 +1,4 @@
+const uuid = require("uuid");
 
-
-var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "1", "2", "3", "4", "5"];
-
-var random_string = ""
-
-for(let i=0; i<6; i++)
-{
-    let temp = characters[Math.floor(Math.random()*13)];
-    random_string += temp;
-}
-console.log(random_string);
+let id = uuid.v1();
+console.log(id);
