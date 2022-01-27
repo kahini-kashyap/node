75217ff6b2a80c66d2be3e13c2efd1407ef999fb
Kahini Kashyap <jili@Kahinis-MacBook-Air.local>
2022-01-27 18:46:01 +0530

    27/1/22

--- Projects/bingomulti/app.js
+++ Projects/bingomulti/app.js
@@ -6,34 +6,42 @@ const wss = new ws.Server({ port: 5000 });
 
 
 wss.on("connection", function (client) {
-    console.log(wss.clients.size);
     if (wss.clients.size > 2) {
         let notification = { type: 1, message: "room is occupied by 2 players", status: 0 };
         client.send(JSON.stringify(notification));
     }
     else if (wss.clients.size == 1) {
-        let notification = { type: 1, message: "waiting for second player", status: 0 };
+        let notification = { type: 1, message: "waiting for second player", status: 0};
         client.send(JSON.stringify(notification));
     }
     else if (wss.clients.size == 2) {
+        client.send(JSON.stringify({ type: 1, message: "second player joined, please start the game", status: 1}))
         wss.clients.forEach(function (player) {
-            let notification = { type: 1, message: "second player joined, start the game", status: 1, };
-            player.send(JSON.stringify(notification));
+            let notification = { type: 1, message: "second player joined, please start the game", status: 1};
+            if (player != client) {
+                player.send(JSON.stringify(notification));
+            }
         })
     }
 
-    client.onmessage = function(message){
+    client.onmessage = function (message) {
         wss.clients.forEach(function (player) {
-            if(player!=client){
-            player.send(message.data);
-        }
+            if (player != client) {
+                player.send(message.data);
+            }
         })
     }
 
 })
 
+// app.get("/curplayer", function(req, res){
+//     res.json({
+
+//     })
+// })
+
 app.get("/", function (req, res) {
-    res.render("index.ejs");
+    res.render("index.ejs",{player_number:wss.clients.size});
 })
 
 app.listen(3000, function (err) {
